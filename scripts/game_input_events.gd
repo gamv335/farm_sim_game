8d5a31eebb64f30c9157722bd1727d2e79a67043
gamv335 <63130934+gamv335@users.noreply.github.com>
2026-03-29 21:00:20 +0100

    Add files via upload

---
+++ scripts/game_input_events.gd
@@ -0,0 +1,30 @@
+# Created class to store input event methods
+class_name GameInputEvents
+
+#Ensure we got a variable to store direction
+static var direction: Vector2
+
+static func movement_input() -> Vector2:
+	if Input.is_action_pressed("walk_up"):
+		direction = Vector2.UP
+	elif Input.is_action_pressed("walk_down"):
+		direction = Vector2.DOWN
+	elif Input.is_action_pressed("walk_left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_pressed("walk_right"):
+		direction = Vector2.RIGHT
+	else: 
+		direction = Vector2.ZERO
+	
+	return direction
+
+# Confirms wether we got a direction input
+static func is_movement_input() -> bool:
+	if direction == Vector2.ZERO:
+		return false
+	else:
+		return true
+		
+static func use_tool() -> bool:
+	var use_tool_value: bool = Input.is_action_just_pressed("hit")
+	return use_tool_value
